1c67096671245b4a8871b04eb2a10f4f05ce6018
Arnaud Bui <arnaud.bui@thomsonreuters.com>
2016-06-15 11:41:30 +0200

    work in progress

--- Data/Shaders/IcosahedronShader.tes.glsl
+++ Data/Shaders/IcosahedronShader.tes.glsl
@@ -15,11 +15,11 @@ out TES_OUT
 
 void main()
 {
-	vec4 p0 = gl_TessCoord.x * gl_in[0].gl_Position;
-    vec4 p1 = gl_TessCoord.y * gl_in[1].gl_Position;
-    vec4 p2 = gl_TessCoord.z * gl_in[2].gl_Position;
+	vec3 p0 = gl_TessCoord.x * gl_in[0].gl_Position.xyz;
+    vec3 p1 = gl_TessCoord.y * gl_in[1].gl_Position.xyz;
+    vec3 p2 = gl_TessCoord.z * gl_in[2].gl_Position.xyz;
 
-    gl_Position = normalize(p0 + p1 + p2);
+    gl_Position = vec4(normalize(p0 + p1 + p2), 1.f);
 	tes_out.InstanceId = tes_in[0].InstanceId;
     //tes_out.PatchDistance = gl_TessCoord;
 }
