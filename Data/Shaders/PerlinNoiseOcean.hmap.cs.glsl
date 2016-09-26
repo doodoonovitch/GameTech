a73db70f98a4accef0e661a2f94f6088f360f629
Arnaud Bui <arnaud.bui@gmail.com>
2016-09-26 18:04:43 +0200

    In PerlinNoiseOceanRenderer shader, precompute ocean height map by using Compute Shader. (work in progress)

--- Data/Shaders/PerlinNoiseOcean.hmap.cs.glsl
+++ Data/Shaders/PerlinNoiseOcean.hmap.cs.glsl
@@ -1,15 +1,38 @@
 #version 450 core
 
-layout (local_size_x = 64, local_size_y = 64) in;
+struct Param
+{
+	vec2 u_A; // xy : Direction, 
+	vec3 u_B; // x : wave length, y : amplitude, z : velocity
+};
 
-layout (binding = 0) readonly uniform image2D image_in;
-layout (binding = 1, r32f) writeonly uniform image2D image_out;
+uniform int u_WaveCount;
+uniform ivec2 u_TextureSize;
+uniform float u_Time;
+
+layout (binding = 0, r32f) writeonly uniform image2D image_out;
+
+layout (binding = 1) readonly uniform image2D image_in;
+layout (binding = 2, std430) uniform Params
+{
+	Param u_WaveParam[];
+};
+
+
+layout (local_size_x = 64, local_size_y = 64) in;
 
 void main(void)
 {
-	vec4 texel;
 	ivec2 p = ivec2(gl_GlobalInvocationID.xy);
-	texel = imageLoad(image_in, p);
-	texel = vec4(1.0) - texel;
-	imageStore(image_out, p, texel);
+	vec2 tc = p / u_TextureSize;
+
+	float H = 0;
+
+	for(int i = 0; i < u_WaveCount; ++i)
+	{
+		vec2 uv = tc * u_WaveParam[i].u_B.x + u_Time * u_WaveParam[i].u_B.z * u_WaveParam[i].u_A.xy;
+		H += u_WaveParam[i].u_B.y * imageLoad(image_in, ivec2(round(uv * u_TextureSize))).r;
+	}		 
+
+	imageStore(image_out, p, H);
 }
